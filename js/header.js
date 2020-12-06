52baf9d42d6ae7a753b193c3eeffabe005d3b00a
izeal <ivanzotov174@gmail.com>
2020-12-06 21:12:32 +0300

    update

---
+++ js/header.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.button-link').click(function(){
+    var id = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: ($(id).offset().top - 90)
+    }, 500);
+    return false;
+  });
+});
